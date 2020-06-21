db17666425194276ae1e3e49e54e9244f5c58dc6
D Surya Sai <dsuryasaidss@gmail.com>
2020-06-21 11:20:05 +0530

    Add files via upload

---
+++ Laravel_RESTful_Controllers+CRUD/app/Http/Controllers/UserController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('User.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('User.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $request->validate([
+        'name'=>'required|max:100',
+        'email'=> 'required|email|max:100'
+      ]);
+
+      $user        = new User;
+      $user->name  = $request->name;
+      $user->email = $request->email;
+      $user->save();
+
+      return redirect('/users')->with('message', 'User has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('User.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+      $request->validate([
+        'name'=>'required|max:100',
+        'email'=> 'required|email|max:100'
+      ]);
+
+      $user        = User::find($id);
+      $user->name  = $request->name;
+      $user->email = $request->email;
+      $user->save();
+
+      return redirect('/users')->with('message', 'User has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect('/users')->with('message', 'User has been deleted');
+    }
+}
